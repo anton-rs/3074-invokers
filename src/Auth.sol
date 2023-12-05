f83c5d146b2ec83e7645ec613925e8c91a11745a
Anna Carroll <anna.s.carroll@gmail.com>
2023-12-05 14:41:20 +0100

    refactor: separate Auth and SimpleAuth

--- src/Auth.sol
+++ src/Auth.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import { SimpleAuth } from "./SimpleAuth.sol";
+
 /// @title Auth
 /// @author Anna Carroll <https://github.com/anna-carroll/3074>
-abstract contract Auth {
+abstract contract Auth is SimpleAuth {
     /// @notice magic byte to disambiguate EIP-3074 signature payloads
     uint8 constant MAGIC = 0x04;
 
-    /// @notice Thrown when the `AUTH` opcode returns an invalid status.
+    /// @notice Thrown when the `AUTH` opcode fails due to invalid signature.
+    /// @dev Selector 0xd386ef3e.
     error BadAuth();
 
-    /// @notice produce a digest for the authorizer to sign
+    /// @notice helper to produce a digest for the authorizer to sign
     /// @param commit - any 32-byte value used to commit to transaction validity conditions
     /// @return digest - sign the `digest` to authorize the invoker to execute the `calls`
     /// @dev signing this digest authorizes address(this) to execute code on behalf of the signer
@@ -28,37 +31,22 @@ abstract contract Auth {
     /// @notice call AUTH opcode with a given a commitment + signature
     /// @param commit - any 32-byte value used to commit to transaction validity conditions
     /// @dev (v, r, s) are interpreted as an ECDSA signature on the secp256k1 curve over getDigest(commit)
+    /// @custom:reverts BadAuth() if  AUTH fails due to invalid signature
     function auth(address authority, bytes32 commit, uint8 v, bytes32 r, bytes32 s) internal {
-        bytes memory authArgs = abi.encodePacked(yParity(v), r, s, commit);
-        assembly {
-            let success := auth(authority, add(authArgs, 0x20), mload(authArgs))
-            if iszero(success) {
-                // Store the `BadAuth()` selector @ 0x00
-                mstore(0x00, 0xd386ef3e)
-                // Revert with `BadAuth()`
-                revert(0x1C, 0x04)
-            }
-        }
+        bool success = authSimple(authority, commit, v, r, s);
+        if (!success) revert BadAuth();
     }
 
     /// @notice call AUTHCALL opcode with given call instructions
     /// @dev MUST call AUTH before attempting to AUTHCALL
+    /// @custom:reverts with the sub-call revert data if the AUTHCALL fails
     function authCall(address to, bytes memory data, uint256 value, uint256 gasLimit) internal {
+        bool success = authCallSimple(to, data, value, gasLimit);
         assembly {
-            let success := authcall(gasLimit, to, value, 0, add(data, 0x20), mload(data), 0, 0)
             if iszero(success) {
                 returndatacopy(0, 0, returndatasize())
                 revert(0, returndatasize())
             }
-        }
-    }
-
-    /// @dev Internal helper to convert `v` to `yParity` for `AUTH`
-    function yParity(uint8 v) internal pure returns (uint8 yParity_) {
-        assembly {
-            switch lt(v, 35)
-            case true { yParity_ := eq(v, 28) }
-            default { yParity_ := mod(sub(v, 35), 2) }
         }
     }
 }
