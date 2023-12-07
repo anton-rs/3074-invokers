62dfc30813a1906c97787ec9cd79415b9b49fbb6
Anna Carroll <anna.s.carroll@gmail.com>
2023-12-07 23:46:35 +0100

    feat: Executor script

---
+++ script/Execute.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.13;
+
+import { BaseInvoker } from "../src/BaseInvoker.sol";
+import { Script } from "forge-std/Script.sol";
+
+contract Executor is Script {
+    // TODO: access transaction signer of forge script
+    uint256 pk;
+
+    function signAndExecute(address invoker, bytes memory execData) public {
+        // construct the digest from execData
+        bytes32 digest = BaseInvoker(invoker).getDigest(execData);
+        // sign the digest
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(pk, digest);
+        // pack the authData
+        bytes memory authData = abi.encodePacked(vm.addr(pk), v, r, s);
+        // broadcast execute transaction
+        vm.startBroadcast();
+        BaseInvoker(invoker).execute(authData, execData);
+        vm.stopBroadcast();
+    }
+}
